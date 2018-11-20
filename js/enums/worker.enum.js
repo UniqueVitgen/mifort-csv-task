4bb1a6f2ee1c2f27bf15a1393bf6f0647fac6088
vitali <vitgen1997@mail.ru>
2018-11-20 18:29:57 +0300

    typescript

--- js/enums/worker.enum.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var WorkerEnum;
-(function (WorkerEnum) {
-})(WorkerEnum = exports.WorkerEnum || (exports.WorkerEnum = {}));
-//# sourceMappingURL=worker.enum.js.map
--- js/src/test/workers/database.worker.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import * as sinon from 'sinon';
-describe('databaseWorker class', () => {
-    describe('connect function', () => {
-    });
-});
-//# sourceMappingURL=database.worker.spec.js.map
